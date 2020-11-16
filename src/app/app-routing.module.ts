ae6091756f5f0a22b20523d2824fbe44b410a25f
lucashelias <lucas.he.eias@gmail.com>
2020-11-16 07:26:37 -0300

    Login + Usuario + home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuarioComponent } from './cadastros/usuario/usuario.component';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: LoginComponent},
+  {path: 'home' , component: HomeComponent},
+  {path: 'usuario' , component: UsuarioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './cadastros/usuario/usuario.component';
+import { HomeComponent } from './home/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
