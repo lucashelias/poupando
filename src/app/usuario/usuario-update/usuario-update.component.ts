f20449463f36a60289ca8b737b1e7425a37f0a38
lucashelias <lucas.he.eias@gmail.com>
2021-03-02 20:03:20 -0300

    Ajuste update usuario

--- src/app/usuario/usuario-update/usuario-update.component.ts
+++ src/app/usuario/usuario-update/usuario-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario.model';
+import { MensagensPadraoService } from 'src/app/_services/mensagens/mensagens-padrao.service';
+import { UsuarioService } from '../usuario-detalhe/usuario.service';
 
 @Component({
   selector: 'app-usuario-update',
@@ -7,24 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioUpdateComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario
+
+  constructor(private usuarioService: UsuarioService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private mensagemPadrao: MensagensPadraoService) { }
   step = 0;
-  
+
   ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id')
+    this.usuarioService.get(id).subscribe(usuario => {
+      this.usuario = usuario
+    }, error => {
+      console.log(error)
+      this.mensagemPadrao.showMessage('Ocorreu um erro no momento de atualizar o usuário!')
+    })
   }
 
+  atualizarUsuario():void{
+    this.usuarioService.update(this.usuario.id, this.usuario).subscribe(usuario =>{
+      this.mensagemPadrao.showMessage('Usuário atualizado com sucesso!!!')
+      this.router.navigate(['/usuario-detalhe'])
+    }, error =>{
+      console.log(error)
+      this.mensagemPadrao.showMessage('Ocorreu um erro no momento de atualizar o usuário!')
+    })
+  }
 
-setStep(index: number) {
-  this.step = index;
-}
+  cancelar(): void {
+    this.router.navigate(['/usuario-detalhe'])
+  }
 
-nextStep() {
-  this.step++;
-}
+  setStep(index: number) {
+    this.step = index;
+  }
 
-prevStep() {
-  this.step--;
-}
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
 
 
 }
